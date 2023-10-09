df9f4c08ba374acc26811bec2ae200f1223f7c8b
JuanaNataliaGutierrez <naty.rcia33@gmail.com>
2023-10-08 21:34:17 -0300

    Ticket#2
    Carga datos de video 2 y parte del 3 en css y datos del producto

---
+++ E-commerce/client/js/products.js
@@ -0,0 +1,33 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/banana.png"
+        //muestra numero de producto, nombre, precio
+        //cantidad, y carpeta especifica del producto
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/leche.png",
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img: "/client/media/pollo.png"
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 510,
+        quanty: 1,
+        img: "/client/media/mayonesa.png"
+    },
+
+];
